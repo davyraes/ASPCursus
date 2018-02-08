fa9e348e94e8bdfea97d13d30ea6324c3a6b5aed
davyraes <davy-raes@telenet.be>
2018-02-08 09:52:48 +0100

    Add project files.

---
+++ ASPCursus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPCursus.Startup))]
+namespace ASPCursus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
